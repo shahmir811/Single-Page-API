502d6f044c1f4c463fb28e6281900e38a2954212
shahmir811 <shahmirkj@gmail.com>
2017-01-13 12:52:47 +0500

    4-SIGN IN with JWT Authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-
+use JWTAuth;
+use Carbon\Carbon;
 use App\Models\User;
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Http\Requests\Auth\RegisterFormRequest;
 
 class AuthController extends Controller
@@ -19,4 +21,29 @@ class AuthController extends Controller
       ]);
 
     }
+
+    public function signin(Request $request)
+    {
+        try {
+            $token = JWTAuth::attempt($request->only('email', 'password'), [
+                'exp' => Carbon::now()->addWeek()->timestamp,
+            ]);
+
+        } catch (JWTException $e)
+        {
+            return response()->json([
+                'error' => 'Could Not Authenticate',
+            ], 500);
+        }
+
+        if(!$token)
+        {
+            return response()->json([
+                'error' => 'Could Not Authenticate',
+            ], 401);
+        }
+
+        return response() -> json(compact('token'));
+    }
+
 }
