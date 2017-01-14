0734ad4acd355d1e60481c2685e45e8d4d0059f8
shahmir811 <shahmirkj@gmail.com>
2017-01-14 09:02:30 +0500

    7-Building up Sections

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 
 Route::post('/auth/signup', ['uses' => 'AuthController@signup']);
 Route::post('/auth/signin', ['uses' => 'AuthController@signin']);
+Route::get('/sections', ['uses' => 'Forum\SectionController@index']);
 
 Route::group(['middleware' => 'jwt.auth'], function() {
 
